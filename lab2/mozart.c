0993a26954de9073d1d85f93906c462f36e278d0
Error-Undefined <ottome@live.com>
2019-05-16 21:10:39 +0200

    lab2 code

---
+++ lab2/mozart.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct {
+	double				zero;
+	int				i;
+	char				s[6];
+	char				t[6];
+	unsigned long long		x;
+	unsigned long long		y;
+} a = { 0, 1756, "mozart", "???", 8243124871055238688ull };
+
+size_t strlen(const char* s)
+{
+	int		i;
+	
+	i = 0;
+
+	while (s[i] != 0) // no guarantee that s is 0 terminated?
+			  // s is set to mozart but never null terminated
+		i++;
+
+	return i;
+}
+	
+int main()
+{
+	int	length;
+
+	strncpy(a.t, a.s, 6);
+	
+	length = strlen(a.t);
+
+	printf("a.t is %s with length %d\n", a.t, length);
+
+	return 0;
+}
